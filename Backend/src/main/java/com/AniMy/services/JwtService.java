c6bd8b7bb2e222251636a68e28b3319ff292aeb5
20236051@stud.fci-cu.edu.eg <sherifabdulmaged@gmail.com>
2025-04-18 15:28:03 +0200

    pass app

---
+++ Backend/src/main/java/com/AniMy/services/JwtService.java
@@ -0,0 +1,7 @@
+package com.AniMy.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+}
--- Backend/src/main/java/com/AniMy/services/MailSenderService.java
+++ Backend/src/main/java/com/AniMy/services/MailSenderService.java
@@ -5,6 +5,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,29 +19,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Service
-@AllArgsConstructor
 public class MailSenderService implements EmailSender {
+
     private final static Logger logger = LoggerFactory.getLogger(MailSenderService.class);
     private final JavaMailSender javaMailSender;
+
+    public MailSenderService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Value("${spring.mail.username}")
+    private String username;
 
     @Override
     @Async
     public void send(String to, String email) {
-        try{
+        try {
             MimeMessage message = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper =
-                    new MimeMessageHelper(message,"UTF-8");
-            helper.setText(email, true);
+            MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
+
+            helper.setText(email, true); // HTML content
             helper.setTo(to);
             helper.setSubject("Confirm your email in AniMy");
-            helper.setFrom("11410120236051@stud.cu.edu.eg");
+            helper.setFrom(username);
 
             javaMailSender.send(message);
-        }catch (MessagingException e){
+        } catch (MessagingException e) {
             logger.error("Error sending email: {}", e.getMessage(), e);
             throw new IllegalStateException("Failed to send email", e);
         }
     }
-
-
 }
+
